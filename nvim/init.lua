d7e73c34c46a1fc3c8571d162c48efbc723b1c14
Kenton227 <klrhoden227@gmail.com>
2026-04-26 02:00:41 -0700

    lsp configured and nvim-tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,5 +11,19 @@ vim.opt.shiftwidth = 4
 -- add line numbers
 vim.wo.number = true
 
+-- keymap for clipboard copy/paste
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>p", "\"+p")
+vim.keymap.set("v", "<leader>p", "\"+p")
+
+-- Enable persistent undo
+vim.o.undofile = true
+vim.o.undodir = vim.fn.stdpath("state") .. "/undo"
+-- Create the undodir if it doesn't exist
+if vim.fn.isdirectory(vim.o.undodir) == 0 then
+    vim.fn.mkdir(vim.o.undodir, "p")
+end
+
 -- source nvim/lua/config/lazy.lua
 require("config.lazy")
--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -3,12 +3,12 @@ local M = {}
 function M.setup()
     local opts = {
         ensure_installed = {
-            "lua_ls",   -- Lua lsp
-            "clangd",   -- C/C++ lsp
-            "pylsp",    -- Python lsp
-            "jdtls",    -- Java lsp
-            "prettier", -- Java formatter
-            "marksman", -- Markdown
+            "lua_ls",       -- Lua
+            "clangd",       -- C/C++
+            "pylsp",        -- Python
+            "jdtls",        -- Java
+            "marksman",     -- Markdown
+            "ts_ls",        -- TS/JS
         },
     }
     require("mason-lspconfig").setup(opts)
