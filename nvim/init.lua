e209df96cdc74a537165bd19eb44f85cf42ab41f
Kenton227 <klrhoden227@gmail.com>
2026-04-26 23:16:43 -0700

    add keymap for rename symbol using lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,6 +22,8 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
+-- LSP rename symbol under cursor
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
 
 -- Enable persistent undo
 vim.o.undofile = true
