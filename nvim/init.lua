62dc93c0d5a3803592d95ba1c7b9780c01b1c325
Kenton227 <klrhoden227@gmail.com>
2026-04-27 01:52:48 -0700

    navigation keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,7 +9,9 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
 -- add line numbers
-vim.wo.number = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 
 -- keymaps
 -- clipboard copy/paste
@@ -22,6 +24,20 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
+-- tab navigation
+vim.keymap.set("n", "<leader>n", "<cmd>tabnew<CR>",   { desc = "Tab: New" })
+vim.keymap.set("n", "<leader>q", "<cmd>tabclose<CR>", { desc = "Tab: Close" })
+vim.keymap.set("n", "<leader>o", "<cmd>tabonly<CR>",  { desc = "Tab: Only" })
+vim.keymap.set("n", "<S-l>", "<cmd>tabnext<CR>", { desc = "Tab: Next" })
+vim.keymap.set("n", "<S-h>", "<cmd>tabprev<CR>", { desc = "Tab: Prev" })
+for i = 1, 9 do
+  vim.keymap.set("n", "<leader>" .. i, i .. "gt", { desc = "Tab: Go to " .. i })
+end
+-- resize split
+vim.keymap.set("n", "<A-h>", "<cmd>vertical resize -5<CR>", { desc = "Resize split left" })
+vim.keymap.set("n", "<A-l>", "<cmd>vertical resize +5<CR>", { desc = "Resize split right" })
+vim.keymap.set("n", "<A-j>", "<cmd>resize -2<CR>",          { desc = "Resize split down" })
+vim.keymap.set("n", "<A-k>", "<cmd>resize +2<CR>",          { desc = "Resize split up" })
 -- LSP rename symbol under cursor
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
 
--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -15,6 +15,10 @@ return {
                 nowait = true,
                 desc = "CD into node",
             })
+            -- keymaps for changing split
+            local opts = { buffer = bufnr, noremap = true, silent = true, nowait = true }
+            vim.keymap.set("n", "<S-h>", "<cmd>tabprev<CR>", opts)
+            vim.keymap.set("n", "<S-l>", "<cmd>tabnext<CR>", opts)
         end,
     },
 }
